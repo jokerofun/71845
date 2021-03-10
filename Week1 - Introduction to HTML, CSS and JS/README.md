5418d3b758e8760fc51da692259a274f6c2ecfdd
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-03-10 16:46:30 +0200

    add solutions task 2

--- Week1 - Introduction to HTML, CSS and JS/README.md
+++ Week1 - Introduction to HTML, CSS and JS/README.md
@@ -1,4 +1,9 @@
-# Task
+# Task 1
 Create the following UI.
 
-![Alt text](ui.png?raw=true "ui.png")
+![Alt text](pictures/ui_task1.png?raw=true "ui_task1.png")
+
+# Task 2
+Create the following UI.
+
+![Alt text](pictures/ui_task2.png?raw=true "ui_task2.png")
---
+++ Week1 - Introduction to HTML, CSS and JS/task2/main.js
@@ -0,0 +1,46 @@
+function btnCalculateOnAction() {
+    const body = document.body;
+
+    const arg1 = body.querySelector('#arg1');
+    const arg2 = body.querySelector('#arg2');
+
+    if (arg1.value.trim() == '' || arg2.value.trim() == '') {
+        alert('Whitespace is not allowed!');
+        return;
+    }
+
+    const number1 = Number(arg1.value);
+    const number2 = Number(arg2.value);
+
+    if (typeof number1 === 'NaN' || typeof number2 !== 'NaN') {
+        alert('Only numbers allowed!');
+        return;
+    }
+
+    switch (document.querySelector('select').selectedIndex) {
+        case 0: {
+            alert(number1 + number2);
+            break;
+        }
+        case 1: {
+            alert(number1 - number2);
+            break;
+        }
+        case 2: {
+            alert(number1 * number2);
+            break;
+        }
+        case 3: {
+            if (number2 == 0) {
+                alert('Division by zero!');
+                return;
+            }
+
+            alert(number1 / number2);
+            break;
+        }
+        default: {
+            alert('Invalid operator!');
+        }
+    }
+}
