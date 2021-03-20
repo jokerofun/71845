edccfe6794c4f41e8c70064bbe988fc46d7306a3
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-03-20 23:41:27 +0200

    add sing-in and begin main

--- Project_Sandbox/mapster/frontend/src/app/app-routing.module.ts
+++ Project_Sandbox/mapster/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: '', component: LogInComponent}
+  { path: '', pathMatch: 'full', component: LogInComponent},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'main', component: MainComponent},
 ];
 
 @NgModule({
--- Project_Sandbox/mapster/frontend/src/app/app.module.ts
+++ Project_Sandbox/mapster/frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    SignInComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
