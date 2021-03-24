75ab95b2ef9fe654abc7e2d36d2e2df7a5f80900
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-03-24 22:26:50 +0200

    add sidemenu, fix padding and rename sign in route

--- Project_Sandbox/mapster/frontend/src/app/app-routing.module.ts
+++ Project_Sandbox/mapster/frontend/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LogInComponent},
-  { path: 'sign-in', component: SignInComponent},
+  { path: 'sign-up', component: SignUpComponent},
   { path: 'main', component: MainComponent},
 ];
 
--- Project_Sandbox/mapster/frontend/src/app/app.module.ts
+++ Project_Sandbox/mapster/frontend/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    SignInComponent,
+    SignUpComponent,
     MainComponent
   ],
   imports: [
