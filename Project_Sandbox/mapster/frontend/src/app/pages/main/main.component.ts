0a961ef021e69e95d7e1bf73d519ea69d3b6305e
Simeon Kolev <simeonkolev99@gmail.com>
2021-05-02 21:52:14 +0200

    My version

--- Project_Sandbox/mapster/frontend/src/app/pages/main/main.component.ts
+++ Project_Sandbox/mapster/frontend/src/app/pages/main/main.component.ts
@@ -11,20 +11,25 @@ import { CircleComponent } from 'src/app/shapes/circle/circle.component';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
   docs: any;
   shapes: any;
 
-  @ViewChild('appendHere', {static : false, read : ViewContainerRef}) target!: ViewContainerRef;
+  shapes2: HTMLElement[] = [];
+
+  @ViewChild('appendHere', { static: false, read: ViewContainerRef })
+  target!: ViewContainerRef;
   private componentRef!: ComponentRef<any>;
 
-  constructor(private docServ: DocumentService, private route: ActivatedRoute, private resolver: ComponentFactoryResolver) { }
+  constructor(
+    private docServ: DocumentService,
+    private route: ActivatedRoute,
+    private resolver: ComponentFactoryResolver
+  ) {}
 
   ngOnInit(): void {
-
     this.route.params.subscribe((params: Params) => {
       console.log(params);
       this.docServ.getShapes(params.docId).subscribe((shapes: any) => {
@@ -46,7 +51,19 @@ export class MainComponent implements OnInit {
   }
 
   generateCircle(): void {
-    const childComponent: ComponentFactory<CircleComponent> = this.resolver.resolveComponentFactory(CircleComponent);
+    // what i've done is I created a new array Shapes2 of type HTMLElement, since that's what you are creating with the next line.
+    // then in the html file i have looped over Shapes2 using ngFor
+    //
+    // Ideally what I think would be best is make the whole canvas/grid/middleColumn thingy a separate component and then in that component..
+    // again have an array e.g. containing objects of type Shape(which can then have x, y, name, etc.), loop through them and create figures(HTMLElement) accordingly.
+    // P.S. I might be mistaken of course, but that's how I would do it in React e.g.
+    // reference I used for this: https://stackoverflow.com/questions/59302528/angular-how-to-dynamically-add-a-list-of-components
+    this.shapes2.push(document.createElement('app-circle'));
+    console.log(this.shapes2);
+
+    const childComponent: ComponentFactory<CircleComponent> = this.resolver.resolveComponentFactory(
+      CircleComponent
+    );
     this.componentRef = this.target.createComponent(childComponent);
   }
 }
